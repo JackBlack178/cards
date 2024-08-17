567a91f24018b1a8c638b7cb17221674480799e9
Ilyas Safiullin <Safiullin1Ilyas@yandex.ru>
2024-08-17 23:40:21 +0300

    Init git

---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import classes from "./Home.module.scss";
+import "../styles/styles.scss";
+import "../styles/_mixins.scss";
+
+const Home = () => {
+  console.log(classes);
+  return <h1>Home</h1>;
+};
+
+export { Home };
