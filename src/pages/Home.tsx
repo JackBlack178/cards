91d1442fefc8a5834b3884f9e9971769dcc5362a
Ilyas Safiullin <Safiullin1Ilyas@yandex.ru>
2024-08-19 19:39:58 +0300

    Header

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,13 @@
 import "../styles/styles.scss";
 import "../styles/_mixins.scss";
-import { Button } from "../components/ui/Button.tsx";
+import Header from "../widgets/Header.tsx";
 
 const Home = () => {
-  return <Button onClick={() => {}}>Hello</Button>;
+  return (
+    <>
+      <Header></Header>
+    </>
+  );
 };
 
 export { Home };
--- src/widgets/Header.tsx
+++ src/widgets/Header.tsx
@@ -1,5 +1,73 @@
+import cl from "./Header.module.scss";
+import { Logo } from "../components/simple/Logo.tsx";
+import { Link } from "react-router-dom";
+import { Button } from "@mui/material";
+import { clsx } from "clsx";
+import { useRef } from "react";
+import { Menu } from "../components/simple/Menu.tsx";
+
 const Header = () => {
-  return <div></div>;
+  const modalRef = useRef<HTMLDialogElement>(null);
+
+  function handleMenuClick() {
+    if (modalRef.current) {
+      modalRef.current.showModal();
+    }
+  }
+
+  return (
+    <header className={cl.Header}>
+      <div className={cl.Header__inner}>
+        <Logo className={cl.Header__logo}></Logo>
+        <nav className={clsx(cl.Header__nav, "hidden-mobile")}>
+          <ul className={cl.Header__nav_list}>
+            <li className={cl.Header__nav_item}>
+              <Link to="/" className={cl.Header__nav_link}>
+                Home
+              </Link>
+            </li>
+            <li className={cl.Header__nav_item}>
+              <Link to="/" className={cl.Header__nav_link}>
+                Articles
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <Button className={clsx(cl.Header__button, "hidden-mobile")}>
+          Sign in
+        </Button>
+        <button
+          onClick={handleMenuClick}
+          className={clsx(cl.Header__button_mobile, "visible-mobile")}
+        >
+          <Menu className={cl.Header__menu}></Menu>
+        </button>
+      </div>
+      <dialog className={clsx(cl.Header__dialog)} ref={modalRef}>
+        <form className={cl.Header__dialog_form} method="dialog">
+          <button
+            type="submit"
+            className={cl.Header__dialog_button_close}
+          ></button>
+        </form>
+        <div className={cl.Header__dialog_inner}>
+          <Button className={clsx(cl.Header__dialog_button)}>Sign in</Button>
+          <ul className={cl.Header__dialog_list}>
+            <li className={cl.Header__dialog_item}>
+              <Link to="/" className={cl.Header__dialog_link}>
+                Home
+              </Link>
+            </li>
+            <li className={cl.Header__dialog_item}>
+              <Link to="/" className={cl.Header__dialog_link}>
+                Articles
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </dialog>
+    </header>
+  );
 };
 
 export default Header;
