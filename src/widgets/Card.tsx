27ba2decc3f596a127e435473f33d582c3e44e94
Ilyas Safiullin <Safiullin1Ilyas@yandex.ru>
2024-08-20 15:08:26 +0300

    Memoized Card component

--- src/widgets/Card.tsx
+++ src/widgets/Card.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, memo } from "react";
 import cl from "./Card.module.scss";
 
 interface CardProp {
@@ -11,35 +11,31 @@ interface CardProp {
   category?: string;
 }
 
-const Card: FC<CardProp> = ({
-  title,
-  body,
-  number,
-  url,
-  imageUrl,
-  category,
-  rating,
-}) => {
-  return (
-    <article className={cl.Article}>
-      <img className={cl.Article__image} src={imageUrl} alt="" />
-      <div className={cl.Article__body}>
-        <span className={cl.Article__category}>{category}</span>
-        <h3 className={cl.Article__title}>
-          {number}. {title}
-        </h3>
-        <div className={cl.Article__text}>
-          <p>{body}</p>
+const Card: FC<CardProp> = memo(
+  ({ title, body, number, url, imageUrl, category, rating }) => {
+    console.log("rendering Card");
+
+    return (
+      <article className={cl.Article}>
+        <img className={cl.Article__image} src={imageUrl} alt="" />
+        <div className={cl.Article__body}>
+          <span className={cl.Article__category}>{category}</span>
+          <h3 className={cl.Article__title}>
+            {number}. {title}
+          </h3>
+          <div className={cl.Article__text}>
+            <p>{body}</p>
+          </div>
         </div>
-      </div>
-      <div className={cl.Article__extra}>
-        <span>Рейтинг: {rating}</span>
-        <a className={cl.Article__link} href={url} target="_blank">
-          Узнать больше
-        </a>
-      </div>
-    </article>
-  );
-};
+        <div className={cl.Article__extra}>
+          <span>Рейтинг: {rating}</span>
+          <a className={cl.Article__link} href={url} target="_blank">
+            Узнать больше
+          </a>
+        </div>
+      </article>
+    );
+  },
+);
 
 export { Card };
